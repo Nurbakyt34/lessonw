c8062cf60d8b63da812b00d4db50f65943a69f64
Nurbakyt <n79907561@gmail.com>
2024-11-07 09:56:52 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,85 @@
+
+const form = document.getElementById('locationInput'); 
+const cityName = document.querySelector('.name'); 
+const temperature = document.querySelector('.temp'); 
+const timeElement = document.querySelector('.time'); 
+const dateElement = document.querySelector('.date'); 
+const conditionElement = document.querySelector('.condition'); 
+const tempMaxElement = document.querySelector('.temp-max'); 
+const tempMinElement = document.querySelector('.temp-min'); 
+const humidityElement = document.querySelector('.humidity'); 
+const cloudyElement = document.querySelector('.cloud'); 
+const windElement = document.querySelector('.wind'); 
+const weatherIcon = document.querySelector('.icon'); 
+const weatherApp = document.querySelector('.weather-app');
+
+const apiKey = '2a44318a88c074e4bdbd09721e9f08b6'; 
+const base_url = 'https://api.openweathermap.org/data/2.5/weather?q='; 
+
+form.addEventListener('submit', async (e) => { 
+    e.preventDefault(); 
+    const location = form.querySelector('.search').value; 
+
+    if (!location) return; 
+
+    const weatherData = await getWeatherData(location); 
+    updateWeatherUI(weatherData); 
+}); 
+
+async function getWeatherData(location) { 
+    const response = await fetch(`${base_url}${location}&appid=${apiKey}&units=metric`); 
+    const data = await response.json(); 
+    return data; 
+} 
+  
+function updateWeatherUI(data) { 
+    if (data.cod === '404') { 
+        alert('Город не найден!'); 
+        return; 
+    } 
+
+    const { main, weather, wind, name, dt } = data; 
+    const date = new Date(dt * 1000); 
+
+    cityName.textContent = name.toUpperCase(); 
+    temperature.textContent = Math.round(main.temp) + '°'; 
+    tempMaxElement.textContent = Math.round(main.temp_max) + '°'; 
+    tempMinElement.textContent = Math.round(main.temp_min) + '°'; 
+    humidityElement.textContent = main.humidity + '%'; 
+    cloudyElement.textContent = weather[0].description.charAt(0).toUpperCase() + weather[0].description.slice(1); 
+    windElement.textContent = wind.speed + ' km/h'; 
+    conditionElement.textContent = weather[0].main; 
+
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }; 
+    dateElement.textContent = date.toLocaleDateString('en-US', options); 
+    timeElement.textContent = date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }); 
+
+    weatherIcon.src = `https://openweathermap.org/img/wn/${weather[0].icon}@2x.png`; 
+
+    updateBackground(weather[0].main);
+}
+function updateBackground(condition) {
+    weatherApp.classList.remove('rainy', 'snowy', 'cloudy', 'clear','smoke','fog','mist');
+
+    switch (condition.toLowerCase()) {
+        case 'clear':
+            weatherApp.classList.add('clear');
+            break;
+       
+       
+    }
+}
+  
+form.addEventListener('submit', async (e) => {  
+    e.preventDefault();  
+    const location = form.querySelector('.search').value;  
+    
+    if (!location) return;  
+    
+    const weatherData = await getWeatherData(location);  
+    updateWeatherUI(weatherData);  
+    
+    form.querySelector('.search').value = '';  
+});  
+getWeatherData('Bishkek').then(updateWeatherUI);
+
